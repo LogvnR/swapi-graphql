c71799e2d34e534e7daba53bedce258dbbd29f09
Logan Ricard <logvnjs@gmail.com>
2022-12-20 14:04:09 -0500

    updated home page and routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,28 @@
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 import App from './App';
+import Movies from './pages/Movies';
 
 const client = new ApolloClient({
   uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/movies',
+    element: <Movies />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ApolloProvider client={client}>
-    <App />
+    <RouterProvider router={router} />
   </ApolloProvider>
 );
